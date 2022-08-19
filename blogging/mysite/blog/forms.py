aee6ab07548cef2a527b6e44eacdef3bdd92d8e8
Rudranarayan Muduli <84215414+R-Coder07@users.noreply.github.com>
2022-08-20 00:14:07 +0530

    Delete blogging/mysite directory

--- blogging/mysite/blog/forms.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-
-class RegistrationForm(UserCreationForm):
-    class meta:
-        model = User
-        fields = ['username','password']
